e91150a2952ab2ff830b19f980fec0a961dd6a21
Lilit <qamalyanlilit@gmail.com>
2021-06-15 14:19:15 +0400

    Calculator HMW

---
+++ src/am/lilit/util/Checkstatic.java
@@ -0,0 +1,13 @@
+package am.lilit.util;
+
+public class Checkstatic {
+
+    public static  String static_variable = "hello from variable static";
+    public static void static_method(){
+        System.out.println("Hello i am here");
+
+    }
+    public void nonstatic_method(){
+        System.out.println("hello i am not here");
+    }
+}
